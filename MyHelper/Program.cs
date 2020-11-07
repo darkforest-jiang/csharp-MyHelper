2659b1c4d296e63892514867bc75c69da6256b47
jiangzh <2275502519@qq.com>
2020-11-07 19:41:32 +0800

    添加项目文件。

---
+++ MyHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
